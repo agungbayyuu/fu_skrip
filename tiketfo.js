3fc52335c97be72ed8d4aeda682a16056f433ab1
unknown <agungbayyuu@gmail.com>
2026-02-09 01:43:11 +0700

    fitur tiket fo

---
+++ tiketfo.js
@@ -0,0 +1,44 @@
+function generateTiketFOCut(){
+
+  const nama = document.getElementById('nama_userfo').value;
+
+  if(!nama){
+    alert('Nama customer belum diisi');
+    return;
+  }
+
+  const script =
+`(function () {
+
+  $('#issue_tracker_id').val('1').trigger('change');
+  $('#issue_status_id').val('1').trigger('change');
+  $('#issue_category_id').val('159805').trigger('change');
+
+  $(document).one('ajaxStop', function () {
+
+    $('#issue_subject').val('Link FO Cut - ${nama}');
+
+    $('#issue_description').val(
+\`Dear Team
+
+Terpantau link FO Cut pada site ${nama}.\`
+    );
+
+    $('#issue_assigned_to_id').val('179');
+
+    $('#issue_category_id').val('159805').trigger('change');
+
+    $('#issue_custom_field_values_77').val('CGS');
+
+    console.log('✔️ Ticket FO Cut berhasil digenerate');
+
+  });
+
+})();`;
+
+  scriptTicket.innerText = script;
+
+  urlTicket.innerText =
+  "https://project.dwp.io/projects/link-3rd-party-untuk-user/issues/new";
+
+}
