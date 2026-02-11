be16b68a6c1df63a21a8aa1527ee5bdde15c39b9
agungbayyuu <76136282+agungbayyuu@users.noreply.github.com>
2026-02-11 12:01:22 +0700

    Penambahan kata follow up
    Added additional information regarding electrical issues.

--- followup.js
+++ followup.js
@@ -2,5 +2,5 @@ function generateFollowUp(){
   if(!nama.value) return outFollow.innerText='⚠️ Nama belum diisi';
 
   outFollow.innerText=
-  `Selamat ${getWaktuWIB()}, kami informasikan untuk site ${nama.value} termonitoring down dalam sistem kami.`;
+  `Selamat ${getWaktuWIB()}, kami informasikan untuk site ${nama.value} termonitoring down dalam sistem kami. Apabila ada kendala kelistrikan dapat diinformasikan ke kami. Terimakasih`;
 }
